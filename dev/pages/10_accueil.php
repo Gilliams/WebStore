835714ea26318ffbaaa95dc5a66c3fd022fa467a
Pauline <fel.pauline@gmail.com>
2019-02-20 14:55:04 +0100

    update
    Ajout des dossiers images et de la feuille de style pour la page d'accueil

--- dev/pages/10_accueil.php
+++ dev/pages/10_accueil.php
@@ -1,41 +1,41 @@
 <?php
-include('header.php');
+include('../templates/header.php');
 ?>
-	<main>
+	<main class="accueil">
 		<div class="container-fluid">
 			<div class="row justify-content-center">
 				<h2 class="col-6">Nouveautés films</h2>
 				<div class="slider col-12">
 					<div class="article col-2">
-						<img src="images/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
+						<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
+						<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Films/images.jpg">
+						<img src="../assets/imgs/Films/images.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Films/Wadjda.jpg">
+						<img src="../assets/imgs/Films/Wadjda.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Films/GUY.jpg">
+						<img src="../assets/imgs/Films/GUY.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
@@ -50,35 +50,35 @@ include('header.php');
 				<h2 class="col-6">Nouveautés musique</h2>
 				<div class="slider col-12">
 					<div class="article col-2">
-						<img src="images/Musique/61I-4Zz0d8L._SS500.jpg">
+						<img src="../assets/imgs/Musique/61I-4Zz0d8L._SS500.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Musique/leon-la-bande-originale-01.jpg">
+						<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Musique/ob_5ff67e_52128820.jpg">
+						<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
+						<img src="../assets/imgs/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
 						</div>
 					</div>
 					<div class="article col-2">
-						<img src="images/Musique/51WP-mtlr2L._SS500.jpg">
+						<img src="../assets/imgs/Musique/51WP-mtlr2L._SS500.jpg">
 						<div class="hoverIcons">
 							<span>Rechercher</span>
 							<span>Ajouter au panier</span>
@@ -95,7 +95,7 @@ include('header.php');
 	</main>
 
 	<?php
-	include('footer.php');
+	include('../templates/footer.php');
 	?>
 </body>
 </html>
