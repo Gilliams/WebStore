c3641cd74840af9a1792e1b53d37dd6a47641951
Pauline <fel.pauline@gmail.com>
2019-02-20 14:22:21 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,101 @@
+<?php
+include('header.php');
+?>
+	<main>
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<h2 class="col-6">Nouveautés films</h2>
+				<div class="slider col-12">
+					<div class="article col-2">
+						<img src="images/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Films/images.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Films/Wadjda.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Films/GUY.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+				</div>
+				<div class="sliderButton col-12">
+					<a href=""></a>
+					<a href=""></a>
+					<a href=""></a>
+				</div>
+				<h2 class="col-6">Nouveautés musique</h2>
+				<div class="slider col-12">
+					<div class="article col-2">
+						<img src="images/Musique/61I-4Zz0d8L._SS500.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Musique/leon-la-bande-originale-01.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Musique/ob_5ff67e_52128820.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+					<div class="article col-2">
+						<img src="images/Musique/51WP-mtlr2L._SS500.jpg">
+						<div class="hoverIcons">
+							<span>Rechercher</span>
+							<span>Ajouter au panier</span>
+						</div>
+					</div>
+				</div>
+				<div class="sliderButton col-12">
+					<a href=""></a>
+					<a href=""></a>
+					<a href=""></a>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php
+	include('footer.php');
+	?>
+</body>
+</html>
