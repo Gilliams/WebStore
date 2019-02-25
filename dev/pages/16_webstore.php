c49144052ab8bad74906b9ed01fa352944c36607
Pauline <fel.pauline@gmail.com>
2019-02-25 09:31:28 +0100

    update
    page webstore

---
+++ dev/pages/16_webstore.php
@@ -0,0 +1,49 @@
+<?php
+include('../templates/header.php');
+?>
+	<main class="webstore">
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<h1 class="col-6">Qui sommes-nous?</h1>
+				<div class="col-10">
+					<span>Webstore, votre boutique spécialisée de vente de produits culturels liés au cinéma d’art et d’essai.</span>
+					<p>Dans nos boutiques, tout le cinéma indépendant trouve sa place quel que soit son genre. Les images n’étant rien sans le son qui les accompagne, nous apportons un effort tout particulier pour trouver les meilleures bandes originales ayant ryhtmé nos coups de coeur et décuplé nos émotions.</p>
+				</div>
+				<div class="col-10">
+					<span>Chez Webstore, nous dénichons pour vous toutes les perles rares du cinéma indépendant.</span>
+					<p>Passionnés depuis toujours par les films d’auteurs et les productions d’art et essai, nous souhaitons vous permettre de découvrir (et redécouvrir) anciens trésors et nouvelles pépites. Au travers d’un catalogue varié et en constanteévolution, nous vous accueillons sur notre site ou dans nos boutiques physiques afin de vous faire partagager notre amour du 7ème Art et de ses bandes originales.</p>
+				</div>
+				<h1 class="col-6">Où nous trouver?</h1>
+				<div class="col-10">
+					<span>3 boutiques Webstore pour partager notre passion!</span>
+					<p>Nos boutiques à Paris et Bordeaux vous accueillent du lundi au samedi de 10h à 19H.</p>
+				</div>
+			</div>
+			<div class="row justify-content-between">
+				<div class="col-3">
+					<span>Paris - Montmartre</span>
+					<p>12, rue Muller</p>
+					<p>75018 Paris</p>
+					<p>01.44.27.82.64</p>
+				</div>
+				<div class="col-3">
+					<span>Paris - Quartier latin</span>
+					<p>4, rue Suger</p>
+					<p>75005 Paris</p>
+					<p>01.44.56.72.09</p>
+				</div>
+				<div class="col-3">
+					<span>Bordeaux</span>
+					<p>15, rue des Martyrs</p>
+					<p>33000 Bordeaux</p>
+					<p>05.43.27.82.64</p>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php
+	include('../templates/footer.php');
+	?>
+</body>
+</html>
