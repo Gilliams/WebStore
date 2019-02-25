abdbda939b2c7cf77a20ce6c0369de8dc96a706a
Florian Andrieux (Gilliams) <andrieuxflorian5@gmail.com>
2019-02-25 11:33:57 +0100

    Merge branch 'master' of https://github.com/Gilliams/WebStore

---
+++ dev/pages/14_recherche.php
@@ -0,0 +1,61 @@
+<?php
+include('../templates/header.php');
+?>
+	<main class="recherche">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="resultatsRecherche">Résultats de votre recherche : "Ma recherche"</div>
+			</div>
+			<div class="row justify-content-center">
+				<h1 class="col-6">Films</h1>
+			</div>
+			<div class="row justify-content-center">
+				<div class="article col-2">
+					<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
+					<div class="hoverIcons">
+						<a href=""><span>Rechercher</span></a>
+						<a href=""><span>Ajouter au panier</span></a>
+					</div>
+				</div>
+				<div class="article col-2">
+					<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
+					<div class="hoverIcons">
+						<span>Rechercher</span>
+						<span>Ajouter au panier</span>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<h1 class="col-6">Musique</h1>
+			</div>
+			<div class="row justify-content-center">
+				<div class="article col-2">
+					<img src="../assets/imgs/Musique/61I-4Zz0d8L._SS500.jpg">
+					<div class="hoverIcons">
+						<span>Rechercher</span>
+						<span>Ajouter au panier</span>
+					</div>
+				</div>
+				<div class="article col-2">
+					<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
+					<div class="hoverIcons">
+						<span>Rechercher</span>
+						<span>Ajouter au panier</span>
+					</div>
+				</div>
+				<div class="article col-2">
+					<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
+					<div class="hoverIcons">
+						<span>Rechercher</span>
+						<span>Ajouter au panier</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php
+	include('../templates/footer.php');
+	?>
+</body>
+</html>
