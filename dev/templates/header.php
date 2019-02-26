7df8c668ad0586627e6fc8ecdcff4f6497672bde
Pauline <fel.pauline@gmail.com>
2019-02-26 09:23:41 +0100

    police
    ajout de Libre Franklin via Google Font

--- dev/templates/header.php
+++ dev/templates/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/icones/style.css">
     <link href="https://vjs.zencdn.net/7.4.1/video-js.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:400,700" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
