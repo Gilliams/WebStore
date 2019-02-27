1613afaa59ac17ba2fc089e48d1aa463694b9473
Pauline <fel.pauline@gmail.com>
2019-02-27 10:23:02 +0100

    update
    responsive

--- dev/pages/10_accueil.php
+++ dev/pages/10_accueil.php
@@ -4,39 +4,32 @@ include('../templates/header.php');
 	<main class="accueil">
 		<div class="container-fluid">
 			<div class="row justify-content-center">
-				<h2 class="col-6">Nouveautés films</h2>
-				<div class="slider col-12">
+				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Nouveautés films</h1>
+				<div class="row slider">
 					<div class="icon-Fleche_gauche"><span class="path1"></span><span class="path2"></span></div>
-					<div class="article film col-2">
+					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article film col-2">
+					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article film col-2">
+					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Films/images.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article film col-2">
+					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Films/Wadjda.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article film col-2">
-						<img src="../assets/imgs/Films/GUY.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
@@ -49,39 +42,32 @@ include('../templates/header.php');
 					<a href=""></a>
 					<a href=""></a>
 				</div>
-				<h2 class="col-6">Nouveautés musique</h2>
-				<div class="slider col-12">
+				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Nouveautés musique</h1>
+				<div class="row slider">
 					<div class="icon-Fleche_gauche"><span class="path1"></span><span class="path2"></span></div>
-					<div class="article musique col-2">
+					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Musique/drive.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article musique col-2">
+					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article musique col-2">
+					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 						</div>
 					</div>
-					<div class="article musique col-2">
+					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
 						<img src="../assets/imgs/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article musique col-2">
-						<img src="../assets/imgs/Musique/51WP-mtlr2L._SS500.jpg">
 						<div class="hoverIcons">
 							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
--- dev/pages/14_recherche.php
+++ dev/pages/14_recherche.php
@@ -7,17 +7,17 @@ include('../templates/header.php');
 				<div class="resultatsRecherche">Résultats de votre recherche : "Ma recherche"</div>
 			</div>
 			<div class="row justify-content-center">
-				<h1 class="col-6">Films</h1>
+				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Films</h1>
 			</div>
 			<div class="row justify-content-center">
-				<div class="article col-2">
+				<div class="article col-12 col-sm-6 col-md-4 col-lg-3">
 					<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
 					<div class="hoverIcons">
 						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 					</div>
 				</div>
-				<div class="article col-2">
+				<div class="article col-12 col-sm-6 col-md-4 col-lg-3">
 					<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
 					<div class="hoverIcons">
 						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
@@ -26,24 +26,24 @@ include('../templates/header.php');
 				</div>
 			</div>
 			<div class="row justify-content-center">
-				<h1 class="col-6">Musique</h1>
+				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Musique</h1>
 			</div>
 			<div class="row justify-content-center">
-				<div class="article col-2">
+				<div class="article col-12 col-sm-6 col-md-4 col-lg-3">
 					<img src="../assets/imgs/Musique/drive.jpg">
 					<div class="hoverIcons">
 						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 					</div>
 				</div>
-				<div class="article col-2">
+				<div class="article col-12 col-sm-6 col-md-4 col-lg-3">
 					<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
 					<div class="hoverIcons">
 						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
 						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 					</div>
 				</div>
-				<div class="article col-2">
+				<div class="article col-12 col-sm-6 col-md-4 col-lg-3">
 					<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
 					<div class="hoverIcons">
 						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
