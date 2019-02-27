c62d992bb95856c95913ab2a25fd4c27807c16b5
Pauline <fel.pauline@gmail.com>
2019-02-27 11:06:07 +0100

    Update 10_accueil.php

--- dev/pages/10_accueil.php
+++ dev/pages/10_accueil.php
@@ -5,81 +5,83 @@ include('../templates/header.php');
 		<div class="container-fluid">
 			<div class="row justify-content-center">
 				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Nouveautés films</h1>
-				<div class="row slider">
-					<div class="icon-Fleche_gauche"><span class="path1"></span><span class="path2"></span></div>
-					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
+			</div>
+			<div class="row justify-content-between slider">
+				<div class="icon-Fleche_gauche col-1"><span class="path1"></span><span class="path2"></span></div>
+				<div class="article film col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Films/bwHdTmuk1ImqXhrGtX2HPuSabWr.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 					</div>
-					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Films/images.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article film col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Films/Wadjda.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="icon-Fleche_droite"><span class="path1"></span><span class="path2"></span></div>
 				</div>
-				<div class="sliderButton col-12">
-					<a href=""></a>
-					<a href=""></a>
-					<a href=""></a>
+				<div class="article film col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Films/086055.jpg-c_215_290_x-f_jpg-q_x-xxyxx.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
 				</div>
+				<div class="article film col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Films/images.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
+				</div>
+				<div class="article film col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Films/Wadjda.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
+				</div>
+				<div class="icon-Fleche_droite col-1"><span class="path1"></span><span class="path2"></span></div>
+			</div>
+			<div class="sliderButton row justify-content-center">
+				<a href=""></a>
+				<a href=""></a>
+				<a href=""></a>
+			</div>
+			<div class="row justify-content-center">
 				<h1 class="col-12 col-sm-10 col-md-8 col-lg-6">Nouveautés musique</h1>
-				<div class="row slider">
-					<div class="icon-Fleche_gauche"><span class="path1"></span><span class="path2"></span></div>
-					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Musique/drive.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
+			</div>
+			<div class="row justify-content-between slider">
+				<div class="icon-Fleche_gauche col-1"><span class="path1"></span><span class="path2"></span></div>
+				<div class="article musique col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Musique/drive.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
 					</div>
-					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="article musique col-12 col-sm-6 col-md-4 col-lg-3">
-						<img src="../assets/imgs/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
-						<div class="hoverIcons">
-							<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
-							<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
-						</div>
-					</div>
-					<div class="icon-Fleche_droite"><span class="path1"></span><span class="path2"></span></div>
 				</div>
-				<div class="sliderButton col-12">
-					<a href=""></a>
-					<a href=""></a>
-					<a href=""></a>
+				<div class="article musique col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Musique/leon-la-bande-originale-01.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
 				</div>
+				<div class="article musique col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Musique/ob_5ff67e_52128820.jpg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
+				</div>
+				<div class="article musique col-12 col-sm-6 col-md-4 col-lg-2">
+					<img src="../assets/imgs/Musique/1128-Weinstein-Quentin-Tarantino-Loma-Vista-Announce-Django-Unchained-Soundtrack-1.jpeg">
+					<div class="hoverIcons">
+						<a href=""><span class="icon icon-Details"><span class="path1"></span><span class="path2"></span></span></a>
+						<a href=""><span class="icon icon-Addtocart"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+					</div>
+				</div>
+				<div class="icon-Fleche_droite col-1"><span class="path1"></span><span class="path2"></span></div>
+			</div>
+			<div class="row justify-content-center sliderButton">
+				<a href=""></a>
+				<a href=""></a>
+				<a href=""></a>
 			</div>
 		</div>
 	</main>
